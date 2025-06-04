fc32f9f4cf6b4b5bc3be00a7c686e26fccff2325
Dominick Mancini <dominick.mancini@navcanada.ca>
2025-06-04 20:10:12 +0000

    - Added a .gitignore - Commented out some code

--- overrideablepushbutton.cpp
+++ overrideablepushbutton.cpp
@@ -5,22 +5,22 @@ OverrideablePushButton::OverrideablePushButton(QWidget *parent)
     : QPushButton(parent), _originalParent(parent) {}
 
 void OverrideablePushButton::onParentDisabled(bool disabled) {
-  qInfo() << "Screw you I do what I want!";
-  if (disabled) {
-    // Move to top-level or a different enabled parent
-    this->setParent(nullptr);
-    // Restore geometry relative to original parent
-    if (this->_originalParent) {
-      QPoint globalPos = this->_originalParent->mapToGlobal(this->_originalPos);
-      this->move(globalPos);
-    }
-    show();
-  } else {
-    // Move back to original parent
-    if (this->_originalParent) {
-      this->setParent(this->_originalParent);
-      this->move(this->_originalPos);
-      this->show();
-    }
-  }
+  qInfo() << "Parent disabled!";
+//  if (disabled) {
+//    // Move to top-level or a different enabled parent
+//    this->setParent(nullptr);
+//    // Restore geometry relative to original parent
+//    if (this->_originalParent) {
+//      QPoint globalPos = this->_originalParent->mapToGlobal(this->_originalPos);
+//      this->move(globalPos);
+//    }
+//    show();
+//  } else {
+//    // Move back to original parent
+//    if (this->_originalParent) {
+//      this->setParent(this->_originalParent);
+//      this->move(this->_originalPos);
+//      this->show();
+//    }
+//  }
 }
